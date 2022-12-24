49766bbf2af5b81ed2e8c44e862264f677c1a4a1
Aliyu Kamilu <aliyukamilu002@gmail.com>
2022-12-24 16:13:08 +0100

    first commit

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
-const express = require("express");
-const PORT = process.env.PORT || 3001;
+const express = require("express")
+const PORT = process.env.PORT || 3004;
 const app = express();
-const cors = require("cors");
-const xml2js = require("xml2js");
-const bodyParser = require("body-parser");
+const cors = require("cors")
+const xml2js = require("xml2js")
+const bodyParser = require("body-parser")
+const json2csv = require("json2csv")
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -31,6 +33,49 @@ app.post("/convert-to-json", (req, res) => {
       res.send(result);
     }
   });
+});
+
+app.post('/convert-to-edifact', (req, res) => {
+  // Read the JSON data from the request body
+  const jsonData = req.body
+
+  // Create an EdifactInterchange object
+  const interchange = new edifactLib.EdifactInterchange({
+    sender: jsonData.header.sender,
+    recipient: jsonData.header.recipient,
+  });
+
+  // Create an EdifactMessage object
+  const message = new edifactLib.EdifactMessage({
+    name: jsonData.name,
+  });
+
+  // Add segments to the message
+  for (const segment of jsonData.segments) {
+    message.addSegment({
+      name: segment.name,
+      elements: segment.elements,
+    });
+  }
+
+  // Add the message to the interchange
+  interchange.addMessage(message);
+  // Convert the interchange to an EDIFACT message
+  const edifactMessage = interchange.toString();
+
+  res(edifactMessage)
+
+});
+
+app.post('/convert-to-csv', (req, res) => {
+  // Read the JSON data from the request body
+  const jsonData = req.body;
+
+  // Convert the JSON data to CSV format
+  const csvData = json2csv.parse(jsonData);
+
+  // Send the CSV data as the response
+  res.send(csvData);
 });
 
 app.listen(PORT, () => {
